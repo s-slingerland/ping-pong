80f0bb552c2ed309426d0376226bb123233698f7
s-slingerland <seanasdfgh@gmail.com>
2017-08-26 17:59:38 -0700

    improved functionality, though still buggy

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,31 @@
-$("form#userInputForm").submit(function(event) {
-  var input = $("input#userInput").val();
-});
+//back end----------------
 
+function pong(userInput){
+var result = [];
+for (var a = 1; a <= userInput; a++){
+  if (a % 3 === 0) {
+    result.push("ping");
+  } else if (a % 5 === 0) {
+    result.push("pong");
+  } else if (a % 15 === 0) {
+    result.push("pingpong");
+  } else {
+    result.push(a);
+  }
+}
+return result;
+}
+
+
+
+//front end-------------------
 $(document).ready(function() {
-  $("button#buttonid").click(function() {
-    $("ul.outPut").append("<li>" + input + "</li>");
-     event.preventDefault()
+  $("form#userInputForm").submit(function(event){
+    var uiNumber = parseInt($("input#userInputNumber").val());
+    var result2 = pong(uiNumber);
+    result2.forEach(function(output) {
+      $(".showForm").append("<li>" + output + "<li>");
+      event.preventDefault();
+    });
   });
-
-
-
-})
+});
